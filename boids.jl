cfe8f4df4825aa7db68b666dd192269122848de4
piotr_cenda <piotr.cenda222@gmail.com>
2022-06-03 15:18:22 +0200

    parameters updated

--- boids.jl
+++ boids.jl
@@ -7,10 +7,11 @@ mutable struct Boid
 end
 
 # Parameters
-edgeRadius = 25
-perceptionRadiusMultiplier = 1.8
-maxForce = 5.
-maxSpeed = 20.
+edge_radius = 150
+perception_radius_multiplier = 2.1
+max_force = 5.
+max_speed = 25.
+egde_separation_force = 100000.
 
 # limit the magnitude of a 2-d Array to lim
 function set_limit!(v::Array, lim::Float64)
@@ -36,19 +37,19 @@ end
 ## fly updates the position and velocity of each boid
 # each boid flaps ones per fly update
 function fly!(boid::Boid, flock::Vector{Boid}, alignValue, separationValue, cohesionValue, axis_limit)
-    # maxSpeed = 5.
+    # max_speed = 5.
     flap!(boid, flock, alignValue, separationValue, cohesionValue, axis_limit)
     boid.position += boid.velocity
     boid.velocity += boid.acceleration
-    boid.velocity = set_limit!(boid.velocity, maxSpeed)
+    boid.velocity = set_limit!(boid.velocity, max_speed)
     boid.acceleration .*= 0
 end
 
 # align Boid to its flock 
 function align!(b::Boid, flock::Vector{Boid})
-    # maxForce = 2.
-    # maxSpeed = 5.
-    perceptionRadius = 50 * perceptionRadiusMultiplier
+    # max_force = 2.
+    # max_speed = 5.
+    perceptionRadius = 50 * perception_radius_multiplier
     steering = zeros(size(b.velocity))
     total = 0
 
@@ -62,18 +63,18 @@ function align!(b::Boid, flock::Vector{Boid})
 
     if total > 0
         steering ./= total
-        steering = set_magnitude!(steering, maxSpeed)
+        steering = set_magnitude!(steering, max_speed)
         steering -= b.velocity
-        steering = set_limit!(steering, maxForce)
+        steering = set_limit!(steering, max_force)
     end
     return steering       
 end
 
 # seperate each flock from its flock s.t. they dont collide
 function seperate!(b::Boid, flock::Vector{Boid}, axis_limit)
-    # maxForce = 2.
-    # maxSpeed = 5.
-    perceptionRadius = 50 * perceptionRadiusMultiplier
+    # max_force = 2.
+    # max_speed = 5.
+    perceptionRadius = 50 * perception_radius_multiplier
     steering = zeros(size(b.velocity))
     total = 0
 
@@ -87,53 +88,53 @@ function seperate!(b::Boid, flock::Vector{Boid}, axis_limit)
     end
 
     # custom edge separation
-    if (b.position[1] - axis_limit) < edgeRadius
+    if (b.position[1] - axis_limit) < edge_radius
         plane_dist = [b.position[1] - axis_limit, 0, 0]
         if norm(plane_dist) < perceptionRadius
             diff = plane_dist / (norm(plane_dist)^2)
-            steering += diff.*100
+            steering += diff.*egde_separation_force
             total +=1
         end
     end
-    if (b.position[1] + axis_limit) < edgeRadius
+    if (b.position[1] + axis_limit) < edge_radius
         plane_dist = [b.position[1] + axis_limit, 0, 0]
         if norm(plane_dist) < perceptionRadius
             diff = plane_dist / (norm(plane_dist)^2)
-            steering += diff.*100
+            steering += diff.*egde_separation_force
             total +=1
         end
     end
 
-    if (b.position[2] - axis_limit) < edgeRadius
+    if (b.position[2] - axis_limit) < edge_radius
         plane_dist = [0, b.position[2] - axis_limit, 0]
         if norm(plane_dist) < perceptionRadius
             diff = plane_dist / (norm(plane_dist)^2)
-            steering += diff.*100
+            steering += diff.*egde_separation_force
             total +=1
         end
     end
-    if (b.position[2] + axis_limit) < edgeRadius
+    if (b.position[2] + axis_limit) < edge_radius
         plane_dist = [0, b.position[2] + axis_limit, 0]
         if norm(plane_dist) < perceptionRadius
             diff = plane_dist / (norm(plane_dist)^2)
-            steering += diff.*100
+            steering += diff.*egde_separation_force
             total +=1
         end
     end
 
-    if (b.position[3] - axis_limit) < edgeRadius
+    if (b.position[3] - axis_limit) < edge_radius
         plane_dist = [0, 0, b.position[3] - axis_limit]
         if norm(plane_dist) < perceptionRadius
             diff = plane_dist / (norm(plane_dist)^2)
-            steering += diff.*100
+            steering += diff.*egde_separation_force
             total +=1
         end
     end
-    if (b.position[3] + axis_limit) < edgeRadius
+    if (b.position[3] + axis_limit) < edge_radius
         plane_dist = [0, 0, b.position[3] + axis_limit]
         if norm(plane_dist) < perceptionRadius
             diff = plane_dist / (norm(plane_dist)^2)
-            steering += diff.*100
+            steering += diff.*egde_separation_force
             total +=1
         end
     end
@@ -141,18 +142,18 @@ function seperate!(b::Boid, flock::Vector{Boid}, axis_limit)
 
     if total > 0
         steering ./= total
-        steering = set_magnitude!(steering, maxSpeed)
+        steering = set_magnitude!(steering, max_speed)
         steering -= b.velocity
-        steering = set_limit!(steering, maxForce)
+        steering = set_limit!(steering, max_force)
     end
     return steering       
 end
 
 # follow the flocks average position 
 function cohese!(b::Boid, flock::Vector{Boid})
-    # maxForce = 2.
-    # maxSpeed = 5.
-    perceptionRadius = 100 * perceptionRadiusMultiplier
+    # max_force = 2.
+    # max_speed = 5.
+    perceptionRadius = 100 * perception_radius_multiplier
     steering = zeros(size(b.velocity))
     total = 0
 
@@ -167,9 +168,9 @@ function cohese!(b::Boid, flock::Vector{Boid})
     if total > 0
         steering ./= total
         steering -= b.position
-        steering = set_magnitude!(steering, maxSpeed)
+        steering = set_magnitude!(steering, max_speed)
         steering -= b.velocity
-        steering = set_limit!(steering, maxForce)
+        steering = set_limit!(steering, max_force)
     end
     return steering       
 end
